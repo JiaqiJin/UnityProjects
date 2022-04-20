e7113bb44aa61674dc3e228830385ada757e515a
“Jiaqi_Jin” <fdsam@hotmail.com>
2022-04-20 16:54:38 +0100

    SRP Project

---
+++ SRP/Assets/Custom RP/Runtime/CustomRenderPipelineAsset.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CustomRenderPipelineAsset : RenderPipelineAsset
+{
+	protected override RenderPipeline CreatePipeline()
+	{
+		return null;
+	}
+}
