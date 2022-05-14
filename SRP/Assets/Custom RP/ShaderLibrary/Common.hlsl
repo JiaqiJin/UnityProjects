dfbfb24b2d4061f90d78d1929f5c38f77a1b0e6d
“Jiaqi_Jin” <fdsam@hotmail.com>
2022-05-14 21:38:30 +0100

    instancing

--- SRP/Assets/Custom RP/ShaderLibrary/Common.hlsl
+++ SRP/Assets/Custom RP/ShaderLibrary/Common.hlsl
@@ -2,11 +2,9 @@
 #define CUSTOM_COMMON_INCLUDED
 
 #include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Common.hlsl"
-#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/CommonMaterial.hlsl"
-
 #include "UnityInput.hlsl"
 
-#define UNITY_MATRIX_M unity_ObjectToWorld
+#define UNITY_MATRIX_M unity_ObjectToWorld 
 #define UNITY_MATRIX_I_M unity_WorldToObject
 #define UNITY_MATRIX_V unity_MatrixV
 #define UNITY_MATRIX_VP unity_MatrixVP
--- SRP/Assets/Custom RP/Shaders/UnlitPass.hlsl
+++ SRP/Assets/Custom RP/Shaders/UnlitPass.hlsl
@@ -3,17 +3,37 @@
 
 #include "../ShaderLibrary/Common.hlsl"
 
-CBUFFER_START(UnityPerMaterial)
-	float4 _BaseColor;
-CBUFFER_END
+struct Attributes 
+{
+	float3 positionOS : POSITION;
+	UNITY_VERTEX_INPUT_INSTANCE_ID // When GPU instancing is used the object index is also avaiable as a vertex attribute
+};
 
-float4 UnlitPassVertex(float3 positionOS : POSITION) : SV_POSITION {
-	float3 positionWS = TransformObjectToWorld(positionOS);
-	return TransformWorldToHClip(positionWS);
+struct Varyings
+{
+	float4 positionCS : SV_POSITION;
+	UNITY_VERTEX_INPUT_INSTANCE_ID
+};
+
+UNITY_INSTANCING_BUFFER_START(UnityPerMaterial)
+	UNITY_DEFINE_INSTANCED_PROP(float4, _BaseColor)
+UNITY_INSTANCING_BUFFER_END(UnityPerMaterial)
+
+Varyings UnlitPassVertex(Attributes input) : SV_POSITION
+{
+	Varyings output;
+	UNITY_SETUP_INSTANCE_ID(input); // Extracts the index from the input and store in a global static variable 
+	UNITY_TRANSFER_INSTANCE_ID(input, output); // make the instancie index available in UnlitPassFragment
+	float3 positionWS = TransformObjectToWorld(input.positionOS);
+	output.positionCS = TransformWorldToHClip(positionWS);
+
+	return output;
 }
 
-float4 UnlitPassFragment() : SV_TARGET {
-	return _BaseColor;
+float4 UnlitPassFragment(Varyings input) : SV_TARGET
+{
+	UNITY_SETUP_INSTANCE_ID(input); 
+	return UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _BaseColor);
 }
 
 #endif
